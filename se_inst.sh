751ce514886548a4ce0d86672581ef09034c6001
Paulus13 <59468171+Paulus13@users.noreply.github.com>
2022-03-22 16:59:01 +0300

    Update se_inst.sh

--- se_inst.sh
+++ se_inst.sh
@@ -4,8 +4,8 @@
 #	Works with Ubuntu 18.04 and higher
 
 NET_FORWARD="net.ipv4.ip_forward=1"
-sysctl -w  ${NET_FORWARD}
-sed -i "s:#${NET_FORWARD}:${NET_FORWARD}:" /etc/sysctl.conf
+sudo sysctl -w  ${NET_FORWARD}
+sudo sed -i "s:#${NET_FORWARD}:${NET_FORWARD}:" /etc/sysctl.conf
 
 sudo apt -y install cmake gcc g++ make libncurses5-dev libssl-dev libsodium-dev libreadline-dev zlib1g-dev git
 wget softether-src.tar.gz https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.38-9760-rtm/softether-src-v4.38-9760-rtm.tar.gz
