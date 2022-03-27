f911d5c3371c9f96e63da7412256cb62c17645b4
Paulus13 <59468171+Paulus13@users.noreply.github.com>
2022-03-27 10:17:08 +0300

    Create upd_inst.sh

---
+++ upd_inst.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt install -y unattended-upgrades apt-listchanges
+
+sed -i "s,//Unattended-Upgrade::Remove-Unused-Kernel-Packages \"true\";,Unattended-Upgrade::Remove-Unused-Kernel-Packages \"true\";," /etc/apt/apt.conf.d/50unattended-upgrades 
+sed -i "s,//Unattended-Upgrade::Remove-New-Unused-Dependencies \"true\";,Unattended-Upgrade::Remove-New-Unused-Dependencies \"true\";," /etc/apt/apt.conf.d/50unattended-upgrades
+sed -i "s,//Unattended-Upgrade::Remove-Unused-Dependencies \"false\";,Unattended-Upgrade::Remove-Unused-Dependencies \"true\";," /etc/apt/apt.conf.d/50unattended-upgrades
+sed -i "s,//Unattended-Upgrade::Automatic-Reboot \"false\";,Unattended-Upgrade::Automatic-Reboot \"true\";," /etc/apt/apt.conf.d/50unattended-upgrades 
+sed -i "s,//Unattended-Upgrade::Automatic-Reboot-WithUsers \"true\";,Unattended-Upgrade::Automatic-Reboot-WithUsers \"true\";," /etc/apt/apt.conf.d/50unattended-upgrades
+sed -i "s,//Unattended-Upgrade::Automatic-Reboot-Time \"02:00\";,Unattended-Upgrade::Automatic-Reboot-Time \"02:00\";," /etc/apt/apt.conf.d/50unattended-upgrades
+
+dpkg-reconfigure -plow unattended-upgrades
